1b1821af74cc0499b76ef9a21a273062e4453ae2
owuor91 <owuor91@gmail.com>
2015-05-05 14:53:45 +0300

    Added button for viewing records

--- app/src/main/java/com/example/teeshirt/sqliteapp/MainActivity.java
+++ app/src/main/java/com/example/teeshirt/sqliteapp/MainActivity.java
@@ -4,16 +4,48 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
     DatabaseHelper myDb;
+
+    EditText etName, etSurname, etMarks;
+    Button btnAdd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myDb = new DatabaseHelper(this);
+
+        etName = (EditText)findViewById(R.id.etName);
+        etSurname = (EditText)findViewById(R.id.etSurname);
+        etMarks = (EditText)findViewById(R.id.etMarks);
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+        addData();
+
+    }
+
+    public void addData(){
+        btnAdd.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.insertData(etName.getText().toString(), etSurname.getText().toString(), etMarks.getText().toString());
+                        if(isInserted ==true){
+                            Toast.makeText(MainActivity.this, "Record Inserted", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "Record Insertion Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
     }
 
 
